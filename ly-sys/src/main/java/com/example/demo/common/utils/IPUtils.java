564ce0181bce1c3fa5d48c600dbe023c1cf1394b
ly2012 <ljjly2012@163.com>
2020-04-16 14:58:54 +0800

    二次更新

--- ly-sys/src/main/java/com/example/demo/common/utils/IPUtils.java
+++ ly-sys/src/main/java/com/example/demo/common/utils/IPUtils.java
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
- * https://www.renren.io
- *
- * 版权所有，侵权必究！
- */
-
 package com.example.demo.common.utils;
 
 import com.alibaba.druid.util.StringUtils;
@@ -17,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * IP地址
  *
- * @author Mark sunlightcs@gmail.com
+ * @author
  */
 public class IPUtils {
 	private static Logger logger = LoggerFactory.getLogger(IPUtils.class);
--- ly-sys/src/main/java/com/example/demo/config/MybatisPlusConfig.java
+++ ly-sys/src/main/java/com/example/demo/config/MybatisPlusConfig.java
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
- * https://www.renren.io
- *
- * 版权所有，侵权必究！
- */
-
 package com.example.demo.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
@@ -15,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 /**
  * mybatis-plus配置
  *
- * @author Mark sunlightcs@gmail.com
+ * @author
  */
 @Configuration
 public class MybatisPlusConfig {
