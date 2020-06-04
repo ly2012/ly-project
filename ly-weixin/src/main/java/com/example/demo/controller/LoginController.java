82d67775c4d2ee729a3c6a8c0cc7a75084b4d9ed
Administrator <Administrator@PC-202005271345>
2020-06-04 14:26:10 +0800

    增加微信登录

--- ly-weixin/src/main/java/com/example/demo/controller/LoginController.java
+++ ly-weixin/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,16 +1,26 @@
 package com.example.demo.controller;
 
 import java.io.IOException;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
 
 	@PostMapping("/login")
-	public String login(HttpServletRequest request)throws IOException {
-		
+	public String login(@RequestBody Map<String,String> param)throws IOException {
+		String loginName = param.get("loginName");
+		String password = param.get("password");
+		if(loginName == null || loginName.isEmpty()) {
+			return "nologin";
+		}
+		if(password == null || password.isEmpty()) {
+			return "nopassword";
+		}
 		return "success";
 	}
 }
